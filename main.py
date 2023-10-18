18427983a50d2d011d36e93fbc64212ce0583acf
noah1105 <noahgreen1105@gmail.com>
2023-10-18 16:36:07 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+food_instructions = {
+    "chicken":
+    "1. Preheat the oven to 350°F.\n2. Season the chicken with salt and pepper.\n3. Bake for 45 minutes.",
+    "pasta":
+    "1. Boil water in a large pot.\n2. Add pasta and cook for 8-10 minutes.\n3. Drain and serve with your favorite sauce.",
+    "pizza":
+    "1. Preheat the oven to 450°F.\n2. Place the pizza in the oven and bake for 10-12 minutes."
+}
+
+user_choice = input("Choose a food item (chicken, pasta, pizza): ").lower()
+ 
